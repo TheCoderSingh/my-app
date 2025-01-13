79686fb32539a05941c200d755de784483c8247d
Jaskaran Singh <thecodersingh@gmail.com>
2025-01-13 01:12:05 -0800

    Install & setup redux

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,8 @@
 import { Text, View } from 'react-native';
 import '../../global.css';
 import { useFonts } from 'expo-font';
+import { store } from './store/store';
+import { Provider } from 'react-redux';
 
 export default function Index() {
   const [fontsLoaded] = useFonts({
@@ -13,16 +15,18 @@ export default function Index() {
   }
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text className="text-red-900">
-        Edit app/index.tsx to edit this screen.
-      </Text>
-    </View>
+    <Provider store={store}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Text className="text-red-900">
+          Edit app/index.tsx to edit this screen.
+        </Text>
+      </View>
+    </Provider>
   );
 }
