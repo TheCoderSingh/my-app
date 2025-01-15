4f9fc9d7fc02bdbf46348ab96909e81c6e919b65
Jaskaran Singh <thecodersingh@gmail.com>
2025-01-15 01:18:54 -0800

    Create header

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -1,10 +1,26 @@
-import { Text, SafeAreaView } from 'react-native';
+import { Text, SafeAreaView, View } from 'react-native';
 
 const home = () => {
   return (
-    <SafeAreaView>
-      <Text>home</Text>
-    </SafeAreaView>
+    <View className="bg-[#eb2] h-full">
+      <View
+        className="h-[96px] bg-white flex flex-row justify-center py-8 rounded-b-[30px] "
+        style={{
+          shadowColor: 'rgba(225, 232, 208, 1)',
+          shadowOffset: { width: 0, height: 5 },
+          shadowOpacity: 1,
+          shadowRadius: 38,
+          elevation: 5, // for Android
+        }}
+      >
+        <View>
+          <Text className="text-xl text-center font-body tracking-[-1px]">
+            Match with a:
+          </Text>
+        </View>
+        <View>{/* <Picker /> */}</View>
+      </View>
+    </View>
   );
 };
 
