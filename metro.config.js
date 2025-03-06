756cc8623daae173f6acfc62ba602555de6a586f
shadmanhossain <shadmanh@sfu.ca>
2025-03-05 16:31:15 -0800

    Resolved iOS bundle issues by updating metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,18 +1,21 @@
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require('@expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
-const config = getDefaultConfig(__dirname);
+const defaultConfig = getDefaultConfig(__dirname);
 
 // Ensure the expo-asset plugin is included and allow require.context
-config.transformer = {
+defaultConfig.transformer = {
+  ...defaultConfig.transformer,
   babelTransformerPath: require.resolve('react-native-svg-transformer'),
   assetPlugins: ['expo-asset/tools/hashAssetFiles'],
   unstable_allowRequireContext: true, // Enable require.context
 };
 
-config.resolver = {
-  assetExts: config.resolver.assetExts.filter((ext) => ext !== 'svg'),
-  sourceExts: [...config.resolver.sourceExts, 'svg'],
+defaultConfig.resolver = {
+  ...defaultConfig.resolver,
+  assetExts: defaultConfig.resolver.assetExts.filter((ext) => ext !== 'svg'),
+  sourceExts: [...defaultConfig.resolver.sourceExts, 'svg'],
 };
 
-module.exports = withNativeWind(config, { input: './global.css' });
+// Apply NativeWind
+module.exports = withNativeWind(defaultConfig, { input: './global.css' });
