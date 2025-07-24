748ca50c96c9cd7cf9c8c251e670e892cd7aa19e
Jaskaran <thecodersingh@gmail.com>
2025-07-24 01:50:17 -0700

    Use actual user data in profile and home

--- src/app/features/link-bar/LinkBar.tsx
+++ src/app/features/link-bar/LinkBar.tsx
@@ -4,8 +4,9 @@ import type { LinkProps } from 'expo-router';
 import images from '../../../constants/images';
 
 interface LinkBarProps {
-  href: LinkProps['href'];
+  href: string;
   text: string;
+  className?: string; // Optional className for additional styling
 }
 
 /**
@@ -16,8 +17,9 @@ interface LinkBarProps {
  *
  * @component
  * @param {LinkBarProps} props - The props for the LinkBar component.
- * @param {LinkProps['href']} props.href - The destination URL for the link.
+ * @param {string} props.href - The URL to navigate to when the link is clicked.
  * @param {string} props.text - The display text for the link.
+ * @param {string} [props.className] - Optional additional CSS classes for styling.
  *
  * @returns {JSX.Element} The rendered LinkBar component with a link and an arrow icon.
  *
@@ -26,15 +28,15 @@ interface LinkBarProps {
  *   <LinkBar href="https://example.com" text="Visit Example" />
  * )
  */
-const LinkBar = ({ href, text }: LinkBarProps): JSX.Element => {
+const LinkBar = ({ href, text, className }: LinkBarProps): JSX.Element => {
   return (
     <View
-      className="w-[300px] bg-white px-4 rounded-full h-[24px] py-1"
+      className={`w-[300px] bg-white px-4 rounded-full h-[24px] py-1 ${className}`}
       style={{
         boxShadow: '0px 0px 7px 0px #00000040',
       }}
     >
-      <Link href={href} target="_blank">
+      <Link href={href as any} target="_blank">
         <View className="flex flex-row justify-between items-center w-full">
           <Text className="color-[#828282] font-body text-sm">{text}</Text>
           <Image className="w-3 h-3" source={images.linkArrow} />
--- src/app/features/match/Match.tsx
+++ src/app/features/match/Match.tsx
@@ -37,118 +37,137 @@ const Match = ({ user }: { user: User }): JSX.Element => {
   return (
     <View>
       {/* Name and picture */}
-      <View className="flex flex-row justify-between">
-        <View>
-          <Text className="color-secondary-one font-heading text-lg uppercase">
-            Temporarily Mentor
-          </Text>
-          <Text className="font-heading text-4xl mt-1">{user.name}</Text>
-        </View>
-        <View className="border-4 rounded-full border-secondary-one">
-          <Image
-            height={70}
-            width={70}
-            source={{
-              uri: 'https://avatar.iran.liara.run/public/28',
-            }}
-          />
-        </View>
-      </View>
-
-      {/* Details */}
-      <View>
-        <View className="flex flex-row items-center gap-3">
-          <Work width={20} height={20} />
-          <Text className="font-body text-sm">
-            {user.currentTitle} at {user.currentCompany}
-          </Text>
-        </View>
-        <View className="flex flex-row items-center gap-3 mt-2">
-          <Education width={20} height={20} />
-          <Text className="font-body text-sm">
-            {user.education[0].degree} at {user.education[0].school}
-          </Text>
-        </View>
-        <View className="flex flex-row items-center gap-3 mt-2">
-          <Location width={20} height={20} />
-          <Text className="font-body text-sm">{user.location}</Text>
-        </View>
-        <View className="flex flex-row items-center gap-2 mt-2">
-          <Industry width={20} height={20} />
-          <Text className="font-body text-sm bg-[#73942033] px-2 py-2 rounded-xl">
-            {user.industries[0]}
-          </Text>
-          <Text className="font-body text-sm bg-[#73942033] px-2 py-2 rounded-xl">
-            {user.industries[1] || ''}
-          </Text>
-          <Text className="font-body text-sm bg-[#73942033] px-2 py-2 rounded-xl">
-            {user.industries[2] || ''}
-          </Text>
-        </View>
-      </View>
-
-      {/* Bio */}
-      <View>
-        <Text className="font-body text-xl mt-6">About</Text>
-        <Text className="font-body mt-2 leading-6 tracking-tight">
-          {user.bio}
-        </Text>
-      </View>
-
-      {/* Skills */}
-      <View className="mt-8">
-        <Text className="font-heading text-center mb-4">Skills</Text>
-        <View>
-          {/* Skills with icons go here */}
-          <View className="flex flex-row justify-center gap-x-8">
-            <View className="flex flex-col items-center">
-              <Image source={images.figma} height={55} width={55} />
-              <Text className="font-body text-sm mt-2">Figma</Text>
+          <View className="flex flex-row justify-between">
+            <View>
+              <Text className="font-heading text-4xl mt-1">{user.name}</Text>
             </View>
-            <View className="flex flex-col items-center">
-              <Image source={images.usertesting} height={55} width={55} />
-              <Text className="font-body text-sm mt-2">UserTesting</Text>
-            </View>
-            <View className="flex flex-col items-center">
-              <Image source={images.xd} height={55} width={55} />
-              <Text className="font-body text-sm mt-2">Adobe XD</Text>
-            </View>
-            <View className="flex flex-col items-center">
-              <Image source={images.git} height={55} width={55} />
-              <Text className="font-body text-sm mt-2">GitHub</Text>
+            <View className="border-4 rounded-full border-secondary-one">
+              <Image
+                height={70}
+                width={70}
+                source={{
+                  uri: 'https://avatar.iran.liara.run/public/28',
+                }}
+              />
             </View>
           </View>
-        </View>
-      </View>
 
+          {/* Details */}
+          <View>
+            <View className="flex flex-row items-center gap-3">
+              <Work width={20} height={20} />
+              <Text className="font-body text-sm">
+                {user.currentTitle
+                  ? `${user.currentTitle} at ${user.currentCompany}`
+                  : 'Work not provided'}
+              </Text>
+            </View>
+            <View className="flex flex-row items-center gap-3 mt-2">
+              <Education width={20} height={20} />
+              <Text className="font-body text-sm">
+                {user.education?.[0]
+                  ? `${user.education[0].degree} at ${user.education[0].school}`
+                  : 'Education not provided'}
+              </Text>
+            </View>
+            <View className="flex flex-row items-center gap-3 mt-2">
+              <Location width={20} height={20} />
+              <Text className="font-body text-sm">
+                {user.location || 'Location not provided'}
+              </Text>
+            </View>
+            <View className="flex flex-row items-center gap-2 mt-2">
+              <Industry width={20} height={20} />
+              {user.industries && user.industries.length > 0 ? (
+                user.industries.map((industry, index) => (
+                  <Text
+                    key={index}
+                    className="font-body text-sm bg-[#73942033] px-2 py-2 rounded-xl"
+                  >
+                    {industry}
+                  </Text>
+                ))
+              ) : (
+                <Text className="font-body text-sm pl-1">
+                  Industries not provided
+                </Text>
+              )}
+            </View>
+          </View>
 
-      {/* Portfolio */}
-      <View className="flex flex-row justify-between items-center mt-3">
-        <Text className="font-body">Portfolio:</Text>
-        <LinkBar
-          href={user.website as ExternalPathString}
-          text={user.website || ''}
-        />
-      </View>
+          {/* Bio */}
+          <View>
+            <Text className="font-body text-xl mt-6">About</Text>
+            <Text className="font-body mt-2 leading-6 tracking-tight">
+              {user.bio || 'No bio provided'}
+            </Text>
+          </View>
 
+          {/* Skills */}
+          <View className="mt-8">
+            <Text className="font-heading text-center mb-4">Skills</Text>
+            <View>
+              {/* Skills with icons go here */}
+              <View className="flex flex-row justify-center gap-x-8">
+                {user.skills && user.skills.length > 0 ? (
+                  user.skills.map((skill, index) => (
+                    <View
+                      key={index}
+                      className="flex flex-col items-center"
+                    >
+                      <Image
+                        source={images[skill.toLowerCase() as keyof typeof images]}
+                        height={55}
+                        width={55}
+                      />
+                      <Text className="font-body text-sm mt-2">{skill}</Text>
+                    </View>
+                  ))
+                ) : (
+                  <Text className="font-body text-sm">
+                    No skills provided
+                  </Text>
+                )}
+              </View>
+            </View>
+          </View>
 
-      {/* Contact */}
-      <View className="mt-8 px-6">
-        <View className="flex flex-row items-center justify-center gap-3">
-          <Image source={images.figma} />
-          <LinkBar
-            href="https://linkedin.com/in/john-smith"
-            text="linkedin.com/in/john-smith"
-          />
-        </View>
-        <View className="flex flex-row items-center justify-center gap-3 mt-4">
-          <Image source={images.git} />
-          <LinkBar
-            href="https://github.com/jsssmith"
-            text="github.com/jsssmith"
-          />
-        </View>
-      </View>
+          {/* Contact */}
+          <View className="mt-8 px-3">
+            {/* LinkedIn */}
+            {user.linkedinUrl && (
+              <View className="flex flex-row items-center justify-between gap-3">
+                <Image source={images.linkedinDark} className='w-7 h-7'/>
+                <LinkBar
+                  href={user.linkedinUrl}
+                  text={user.linkedinUrl}
+                />
+              </View>
+            )}
+
+            {/* GitHub */}
+            {user.githubUrl && (
+              <View className="flex flex-row items-center justify-between gap-3 mt-4">
+                <Image source={images.githubDark} className='w-7 h-7'/>
+                <LinkBar
+                  href={user.githubUrl}
+                  text={user.githubUrl}
+                />
+              </View>
+            )}
+
+            {/* Portfolio */}
+            {user.website && (
+             <View className="flex flex-row justify-between items-center mt-4">
+              <Text className="font-body mr-1">Portfolio:</Text>
+              <LinkBar
+                href={user.website}
+                text={user.website}
+                className="flex-1"
+              />
+            </View>
+            )}
+          </View>
 
       {/* Cross and tick buttons */}
       <View>
